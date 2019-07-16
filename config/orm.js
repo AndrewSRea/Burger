bbc20f10f9529adb00b6194c86afc218b39af333
Virtual Drew <andrew.s.rea@gmail.com>
2019-07-16 11:44:53 -0700

    Adding necessary functions

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,27 @@
 var connection = require("../config/connection.js");
 
+function createQmarks(num) {
+    var arr = [];
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
+    }
+    return arr.toString();
+}
+
+function translateSql(ob) {
+    var arr = [];
+    for (var key in ob) {
+        var value = ob[key];
+        if (Object.hasOwnProperty.call(ob, key)) {
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+            arr.push(key + "=" + value);
+        }
+    }
+    return arr.toString();
+}
+
 var orm = {
     selectAll: function(table, cb) {
         var dbQuery = "SELECT * FROM " + table + ";";
@@ -25,6 +47,29 @@ var orm = {
         });
     },
     updateOne: function(table, objColVals, condition, cb) {
-        //don't forget the top of this file
+        var dbQuery =
+        "UPDATE " + table + " SET " + translateSql(objColVals) +
+        " WHERE " + condition;
+
+        console.log(dbQuery);
+
+        connection.query(dbQuery, function(err, res) {
+            if (err) {
+                throw err;
+            }
+            cb(res);
+        });
+    },
+    deleteOne: function(table, condition, cb) {
+        var dbQuery = "DELETE FROM " + table + " WHERE " + condition;
+        console.log(dbQuery);
+
+        connection.query(dbQuery, function(err, res) {
+            if (err) {
+                throw err;
+            }
+            cb(res);
+        });
     }
-}
+};
+module.exports = orm;
